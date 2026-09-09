f828dc4926a7603d1e7935c6378969d4f524aed7
Matheus Gomes Viana <matheusviana996@gmail.com>
2026-09-08 23:35:58 -0300

    end

--- source-after-v225-20260908-232044/external/xess/doc/xess_frame_generation_doxygen/search/all_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['d3d12_20api_20exports_0',['XeSS-FG Swap Chain D3D12 API exports',['../group__xefgswapchain-d3d12.html',1,'']]],
-  ['debug_20features_1',['XeSS-FG Swap Chain debug features',['../group__xefgswapchain__debug.html',1,'']]]
-];
